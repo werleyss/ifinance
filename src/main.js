50062e425cf40addde29b4e225ccea3617649490
werleyss <werley_desouzasilva@hotmail.com>
2021-04-14 00:30:35 -0300

    Implmentando layout e confgurando materialize

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
+import {sync } from 'vuex-router-sync'
+
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+
+Vue.use(Vuex)
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
 import App from './App.vue'
+import routes from './router'
+import VuexStore from './vuex/store'
+
+//Vue.http.option.root = process.env.SERVER
+
+// new Vue({
+//     el: "#app",
+//     router,
+//     template: '<App/>',
+//     components: { App }
+// })
 
 Vue.config.productionTip = false
 
+const store = new Vuex.Store(VuexStore)
+const router = new VueRouter({
+    routes
+})
+
+sync(store, router)
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import HelloWorld from '../components/HelloWorld.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Hello',
+        component: HelloWorld
+    }
+]
+
+export default routes
